65058142597c9fe7ac90f9a7caba5bab0134b7eb
devcui <devcui@outlook.com>
2024-10-08 06:38:00 +0000

    master - fix: fix typedef

--- @yelon/bis/src/startup.service.d.ts
+++ @yelon/bis/src/startup.service.d.ts
@@ -3,9 +3,9 @@ import { Observable } from 'rxjs';
 import { ITokenModel } from '@yelon/auth';
 import { YunzaiMenu } from '@yelon/util';
 import * as i0 from "@angular/core";
-export type LoadParam = {
+export interface LoadParam {
     force?: boolean;
-};
+}
 export declare function provideYunzaiStartup(): Provider[];
 export declare class YunzaiStartupService {
     private readonly config;
